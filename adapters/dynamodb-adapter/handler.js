f7e940a8ec5ddf851f0e44a83b85d4a0f5f773f9
Abraham Cole Finite <abrahamfinite@gmail.com>
2023-12-03 23:42:01 +1100

    babyj-1 : get and remove

--- adapters/dynamodb-adapter/handler.js
+++ adapters/dynamodb-adapter/handler.js
@@ -1,3 +1,5 @@
-import { put } from './lib/items.js';
+import { put, remove, get } from './lib/items.js';
 
-put();
+get("522d0926-5476-403b-adf4-96b3e9d18c3c", "Timmy");
+// put();
+// remove("782b42a0-c684-4876-bbcd-2823036f3b3f");
--- adapters/dynamodb-adapter/lib/items.js
+++ adapters/dynamodb-adapter/lib/items.js
@@ -1,17 +1,55 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, DeleteCommand, GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { v4 as uuidv4 } from 'uuid';
+import 'dotenv/config'
 
-export const put = async() => {
+export const put = async () => {
   const client = new DynamoDBClient({});
   const docClient = DynamoDBDocumentClient.from(client);
 
   const command = new PutCommand({
-    TableName: "weight",
+    TableName: process.env.TABLE_NAME,
     Item: {
       id :  uuidv4(),
       name: "Timmy",
       weight: 1.8,
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+}
+
+export const get = async (itemId, itemName) => {
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+
+  const command = new GetCommand({
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id : itemId,
+      name: itemName
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+}
+
+export const update = async () => {
+
+}
+
+export const remove = async (itemId) => {
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+
+  const command = new DeleteCommand({
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: itemId,
     },
   });
 
