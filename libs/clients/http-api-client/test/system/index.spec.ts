1f9c4e932382b15b54ec0b0f974afa198acd16ff
Abraham Cole Finite <abrahamfinite@gmail.com>
2024-01-22 19:50:15 +1100

    babyj-1 : remove dotenv add env in docker-compose

--- libs/clients/http-api-client/test/system/index.spec.ts
+++ libs/clients/http-api-client/test/system/index.spec.ts
@@ -7,16 +7,16 @@ describe('HttpService', () => {
       console.debug('>>>>>LADBROKES')
       console.debug(process.env.LADBROKES_BEARER_TOKEN)
 
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.LADBROKES_BEARER_TOKEN
-      }
-      const httpApiClient = new HttpApiClient()
-      const result = httpApiClient.get(
-        'https://api.ladbrokes.com.au',
-        'v2/client/PendingBetCount',
-        headers,
-      )
+      // const headers = {
+      //   'Content-Type': 'application/json',
+      //   'Authorization': process.env.LADBROKES_BEARER_TOKEN
+      // }
+      // const httpApiClient = new HttpApiClient()
+      // const result = httpApiClient.get(
+      //   'https://api.ladbrokes.com.au',
+      //   'v2/client/PendingBetCount',
+      //   headers,
+      // )
 
       // console.debug('>>>>>>>>')
       // console.debug(result)
