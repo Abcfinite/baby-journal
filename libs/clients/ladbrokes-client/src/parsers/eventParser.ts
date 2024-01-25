cc30c42eb021e26de6bfe201e6d4e67b1fbf72d7
Abraham Cole Finite <abrahamfinite@gmail.com>
2024-01-25 19:52:10 +1100

    babyj-1 : odd parsed

--- libs/clients/ladbrokes-client/src/parsers/eventParser.ts
+++ libs/clients/ladbrokes-client/src/parsers/eventParser.ts
@@ -9,18 +9,19 @@ export default class EventParser {
     const markets = _.get(bodyJson, 'markets')
     const marketDetails = _.get(markets, mainMarketId)
     const entrantsIds = _.get(marketDetails, 'entrant_ids')
+    const prices = _.get(bodyJson, 'prices')
 
-    console.log('>>>>>entrantsIds')
-    console.log(entrants)
-    console.log(entrantsIds)
-    console.log(_.get(entrants, entrantsIds[0]))
+    const entrant1PriceKey = Object.keys(prices).find(key => key.match(entrantsIds[0]) !== null)
+    const entrant2PriceKey = Object.keys(prices).find(key => key.match(entrantsIds[1]) !== null)
+    const entrant1Odd = _.get(prices, `${entrant1PriceKey}.odds`)
+    const entrant2Odd = _.get(prices, `${entrant2PriceKey}.odds`)
 
     return {
       id: _.get(eventsBody, 'id'),
-      player1: _.get(entrants, `name`, 'please check'),
-      player2: _.get(entrants, `name`, 'please check'),
-      player1Odd: mainMarketId,
-      player2Odd: _.get(bodyJson, 'id'),
+      player1: _.get(entrants, `${entrantsIds[0]}.name`, 'please check'),
+      player2: _.get(entrants, `${entrantsIds[1]}.name`, 'please check'),
+      player1Odd: _.get(entrant1Odd, 'numerator', 0) / _.get(entrant1Odd, 'denominator', 1) + 1,
+      player2Odd: _.get(entrant2Odd, 'numerator', 0) / _.get(entrant2Odd, 'denominator', 1) + 1,
       tournament: _.get(eventsBody, 'competition.name', 'please check'),
     }
   }
