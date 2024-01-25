1b0824fd50dd7e2869a2a4109db8a9ca88e30133
Abraham Cole Finite <abrahamfinite@gmail.com>
2024-01-26 00:05:08 +1100

    babyj-1 : put webpack.config.js

--- libs/domains/sports/bets/index.ts
+++ libs/domains/sports/bets/index.ts
@@ -1,5 +1,23 @@
-import BetAdapter from "../../../adapters/bet-adapter"
+// import BetAdapter from "../../../adapters/bet-adapter"
 
-export const logBets = async () => {
-  await new BetAdapter().logBets()
+import { Handler } from 'aws-lambda';
+
+export const logBets: Handler = async (event: any) => {
+  // await new BetAdapter().logBets()
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'your pending bets stored successfully in dynamodb',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  return new Promise((resolve) => {
+    resolve(response)
+  })
 }
