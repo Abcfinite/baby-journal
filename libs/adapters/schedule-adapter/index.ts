d9ba61ec89c66b7d8d7f7a4a43ae1a1304de0a36
Abraham Cole Finite <abrahamfinite@gmail.com>
2024-03-31 11:03:58 +1100

    sb-14 : Prev player different with WL Rankings

--- libs/adapters/schedule-adapter/index.ts
+++ libs/adapters/schedule-adapter/index.ts
@@ -23,7 +23,7 @@ export default class ScheduleAdapter {
 
     var result
 
-    if (sportEventsNeedCheck.length == 0) {
+    // if (sportEventsNeedCheck.length == 0) {
       const fileContent = []
       await Promise.all(
         fileList.map( async file => {
@@ -32,71 +32,75 @@ export default class ScheduleAdapter {
         })
       )
 
-      const sorted = fileContent.sort((a,b) => {
-        const rankingDifferentA = _.get(a, 'rankingDifferent', 1000)
-        const rankingDifferentB = _.get(b, 'rankingDifferent', 1000)
-
-        return rankingDifferentB - rankingDifferentA
-      })
-
-      return sorted.filter(e => {
+      const filtered = fileContent.filter(e => {
         const wlP1 = _.get(e, 'analysis.winLoseRanking.player1', 0)
         const wlP2 = _.get(e, 'analysis.winLoseRanking.player2', 0)
 
-        const gap = Math.abs(wlP1 - wlP2)
-
-        const p1vp1wonp1 = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p1won.p1', 0)
-        const p2vp1wonp2 = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p1won.p2', 0)
-        const p2vp2wonp2 = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p2won.p2', 0)
-        const p1vp2wonp1 = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p2won.p1', 0)
-        const p1vp1wonp1Won = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p1won.p1Won', 0)
-        const p2vp1wonp2Won = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p1won.p1Won', 0)
-        const p2vp2wonp2Won = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p2won.p2Won', 0)
-        const p1vp2wonp1Won = _.get(e, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p2won.p2Won', 0)
-
-        if (wlP1 === wlP2 ) {
-          return false
-        } else if (wlP1 > wlP2) {
-          console.log('>>a>>', _.get(e, 'player1.name', ''))
-          console.log(gap)
-          console.log((p1vp1wonp1 - p1vp1wonp1Won) - (p2vp1wonp2 - p2vp1wonp2Won))
-          console.log((p1vp2wonp1 - p1vp2wonp1Won) - (p2vp2wonp2 - p2vp2wonp2Won))
-
-          return gap < (p1vp1wonp1 - p1vp1wonp1Won) - (p2vp1wonp2 - p2vp1wonp2Won) ||
-            gap < (p1vp2wonp1 - p1vp2wonp1Won) - (p2vp2wonp2 - p2vp2wonp2Won)
-        } else {
-
-          console.log('>>a>>', _.get(e, 'player1.name', ''))
-          console.log(gap)
-          console.log((p2vp1wonp2 - p2vp1wonp2Won) - (p1vp1wonp1 - p1vp1wonp1Won))
-          console.log((p2vp2wonp2 - p2vp2wonp2Won) - (p1vp2wonp1 - p1vp2wonp1Won))
-
-          return gap < (p2vp1wonp2 - p2vp1wonp2Won) - (p1vp1wonp1 - p1vp1wonp1Won) ||
-            gap < (p2vp2wonp2 - p2vp2wonp2Won) - (p1vp2wonp1 - p1vp2wonp1Won)
-
-        }
+        return wlP1 !== wlP2
       })
 
-      // return sorted
 
-    }
-    else {
-      try {
-        result = await new PlayerAdapter().checkPlayerObject(
-          sportEventsNeedCheck[0].player1, sportEventsNeedCheck[0].player2)
+      const sorted = filtered.sort((a,b) => {
+        // var gapA = this.getGap(a)
+        // var gapB = this.getGap(b)
+        // console.log('>>>>>gapA>>>', gapA)
+        // console.log('>>>>>gapB>>>', gapB)
+        return this.getGap(b) - this.getGap(a)
+      })
 
-        await new S3ClientCustom()
-          .putFile('tennis-match-schedule',
-            sportEventsNeedCheck[0].id+'.json',
-            JSON.stringify(result))
-      } catch(ex) {
-        result = 'error'
-        await new S3ClientCustom()
-          .putFile('tennis-match-schedule',
-            sportEventsNeedCheck[0].id+'.json',
-            JSON.stringify('{}'))
-      }
+      sorted.map(s => {
+        console.log('>>>>gap>>>', s.gap)
+      })
+
+      return sorted
+    // }
+    // else {
+    //   try {
+    //     result = await new PlayerAdapter().checkPlayerObject(
+    //       sportEventsNeedCheck[0].player1, sportEventsNeedCheck[0].player2)
+
+    //     await new S3ClientCustom()
+    //       .putFile('tennis-match-schedule',
+    //         sportEventsNeedCheck[0].id+'.json',
+    //         JSON.stringify(result))
+    //   } catch(ex) {
+    //     result = 'error'
+    //     await new S3ClientCustom()
+    //       .putFile('tennis-match-schedule',
+    //         sportEventsNeedCheck[0].id+'.json',
+    //         JSON.stringify('{}'))
+    //   }
+    // }
+
+    // return result
+  }
+
+  getGap(sportEventAnalysis: {}) {
+    const wlP1 = _.get(sportEventAnalysis, 'analysis.winLoseRanking.player1', 0)
+    const wlP2 = _.get(sportEventAnalysis, 'analysis.winLoseRanking.player2', 0)
+
+    const gap = Math.abs(wlP1 - wlP2)
+
+    const p1vp1wonp1 = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p1won.p1', 0)
+    const p2vp1wonp2 = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p1won.p2', 0)
+    const p2vp2wonp2 = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p2won.p2', 0)
+    const p1vp2wonp1 = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p2won.p1', 0)
+    const p1vp1wonp1Won = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p1won.p1Won', 0)
+    const p2vp1wonp2Won = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p1won.p1Won', 0)
+    const p2vp2wonp2Won = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p2_v_p2won.p2Won', 0)
+    const p1vp2wonp1Won = _.get(sportEventAnalysis, 'analysis.benchmarkPlayer.previousPlayers.numbers.p1_v_p2won.p2Won', 0)
+
+    var cal1 = (p2vp1wonp2 - p2vp1wonp2Won) - (p1vp1wonp1 - p1vp1wonp1Won)
+    var cal2 = (p2vp2wonp2 - p2vp2wonp2Won) - (p1vp2wonp1 - p1vp2wonp1Won)
+
+    if (wlP1 > wlP2) {
+      cal1 = (p1vp1wonp1 - p1vp1wonp1Won) - (p2vp1wonp2 - p2vp1wonp2Won)
+      cal2 = (p1vp2wonp1 - p1vp2wonp1Won) - (p2vp2wonp2 - p2vp2wonp2Won)
     }
+
+    const result = gap - cal1 > gap - cal2 ? gap - cal2 : gap - cal1
+
+    sportEventAnalysis['gap'] = result
 
     return result
   }
