b3dc90a1061f2afcf56fe3e9d5e88031c441925c
Abraham Cole Finite <abrahamfinite@gmail.com>
2024-07-03 09:09:47 +1000

    tips: html to csv

--- libs/adapters/tips-adapter/index.ts
+++ libs/adapters/tips-adapter/index.ts
@@ -2,30 +2,39 @@ import _ from "lodash"
 import { parse } from 'node-html-parser';
 import S3ClientCustom from '@abcfinite/s3-client-custom'
 import BetapiClient from "../../clients/betapi-client";
+import { Prediction } from './src/types/prediction';
 
 export default class TipsAdapter {
   async getTips() {
-    // const matchStatFile = await new S3ClientCustom().getFile('tennis-matchstat', 'matchstat.html')
+    const matchStatFile = await new S3ClientCustom().getFile('tennis-matchstat', 'matchstat.html')
 
-    // const matchStatHtml = parse(matchStatFile)
-    // const predictions = matchStatHtml.getElementsByTagName('div').filter(div => div.attributes.class === 'prediction-table-container')
+    const predictionCols: Array<Prediction> = []
+    const matchStatHtml = parse(matchStatFile)
+    const predictions = matchStatHtml.getElementsByTagName('div').filter(div => div.attributes.class === 'ms-prediction-table')
 
-    // console.log('>>>>>predictions>>>', predictions.length)
+    console.log('>>>>>predictions>>>', predictions.length)
 
-    // predictions.forEach(pred => {
-    //   const pName = pred.querySelector('.player-name-pt');
-    //   console.log('>>>>name>>>>', pName.text)
+    predictions.forEach(pred => {
+      const pTime = pred.querySelector('.prediction-time');
+      const pName = pred.querySelector('.player-name-pt');
+      const aOdds = pred.querySelector('.odds-item.item-border');
+      const predPercentage = pred.querySelector('.prediction-item.item-border');
 
-    //   const aOdds = pred.querySelector('a');
-    //   console.log('>>>>odds>>>>', aOdds.text.replaceAll(/\s/g,''))
+      const prediction: Prediction = {
+        time: pTime.text.replaceAll(/\s/g,''),
+        player1: pName.text.replaceAll(/\s/g,''),
+        odds: aOdds.text.replaceAll(/\s/g,''),
+        percentage: predPercentage.text.replaceAll(/\s/g,'').replaceAll(/\%/g,''),
+      }
 
-    //   const predPercentage = pred.querySelector('.prediction-item.item-border');
-    //   console.log('>>>>odds>>>>', predPercentage.text.replaceAll(/\s/g,'').replaceAll(/\%/g,''))
-    // })
+      if (!prediction.player1.includes('over')) {
+        predictionCols.push(prediction)
+      }
+    })
 
-    new BetapiClient().getEvents()
+    // new BetapiClient().getEvents()
 
-    return  'success'
+    return  predictionCols.map(p => `${p.time},${p.player1},${p.percentage},${p.odds}`).join('\r\n')
 
 
   }
---
+++ libs/adapters/tips-adapter/src/types/prediction.ts
@@ -0,0 +1,6 @@
+export type Prediction = {
+    time: string,
+    player1: string,
+    odds: string,
+    percentage: string,
+  }
